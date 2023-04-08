0275503896194c4d1dd0e7a14599d4e0743aa3b5
Tinsu1923 <130051678+Tinsu1923@users.noreply.github.com>
2023-04-08 14:54:43 -0400

    Arrayed Encrypt-decrypt
    I used an array to save space, and did some filtering out of the codes.

--- encrypt decrypt almost there.cpp
+++ encrypt decrypt almost there.cpp
@@ -1,11 +1,11 @@
 #include<iostream>
 int main(){
     backtofirst:
-    int n,a,b,c,d,e,f,g,h,i,j,k,l,m; // n is for the integer, a,b,c,d is for the individual digits of that integer,i is to show the encrypted number. 
+    int array[10]; 
     char x;   
     do{
         std::cout<<"Enter the four digit number: ";
-        std::cin>>n;
+        std::cin>>array[0];
     }
     while(n<1000 || n>9999);
    std::cout<<"Do you want to encrypt or decrypt the number? "<<std::endl;
@@ -13,49 +13,41 @@ int main(){
    std::cin>>x;
     if(x=='Y' || x=='y')
     {
-       a=n/1000;
-       std::cout<<"The first digit of "<<n<<" is "<<a<<std::endl;
-       b=(n/100)%10;
-       std::cout<<"The second digit of "<<n<<" is "<<b<<std::endl;
-       c=(n/10)%10;
-       std::cout<<"The third digit of "<<n<<" is "<<c<<std::endl;
-       d=n%10;
-       std::cout<<"The fourth digit of "<<n<<" is "<<d<<std::endl;
-       e=(a+7)%10;
-       f=(b+7)%10;
-       g=(c+7)%10;
-       h=(d+7)%10;
-       l=e;
-       m=f;
-       j=g;
-       k=h;
-       std::cout<<"The modulus "<<e<<" "<<f<<" "<<g<<" "<<h<<std::endl;
-       std::cout<<"After the shuffle "<<j<<" "<<k<<" "<<l<<" "<<m<<std::endl;
-       i=(j*1000)+(k*100)+(l*10)+m;
-       std::cout<<"The encrypted number is "<<i<<std::endl;
+       array[1]=array[0] / 1000;
+       array[2]=(array[0] / 100)%10;
+       array[3]=(array[0] / 10)%10;
+       array[4]=array[0] % 10;
+       array[5]=(array[1] + 7)%10;
+       array[6]=(array[2] + 7)%10;
+       array[7]=(array[3] + 7)%10;
+       array[8]=(array[4] + 7)%10;
+       array[11]=array[5];
+       array[12]=array[6];
+       array[9]=array[7];
+       array[10]=array[8];
+       array[13]=(array[9]*1000)+(array[10]*100)+(array[11]*10)+array[12];
+       std::cout<<"The encrypted number is "<<array[13]<<std::endl;
     }
     else if (x=='N' || x=='n')
     {
-       a=n/1000;
+       array[1]=array[0]/1000;
        std::cout<<"The first digit of "<<n<<" is "<<a<<std::endl;
-       b=(n/100)%10;
+       array[2]=(array[0]/100)%10;
        std::cout<<"The second digit of "<<n<<" is "<<b<<std::endl;
-       c=(n/10)%10;
+       array[3]=(array[0]/10)%10;
        std::cout<<"The third digit of "<<n<<" is "<<c<<std::endl;
-       d=n%10;
+       array[4]=array[0]%10;
        std::cout<<"The fourth digit of "<<n<<" is "<<d<<std::endl;
-       e=(a+3)%10;
-       f=(b+3)%10;
-       g=(c+3)%10;
-       h=(d+3)%10;
-       l=e;
-       m=f;
-       j=g;
-       k=h;
-       std::cout<<"The modulus "<<e<<" "<<f<<" "<<g<<" "<<h<<std::endl;
-       std::cout<<"After the shuffle "<<j<<" "<<k<<" "<<l<<" "<<m<<std::endl;
-       i=(j*1000)+(k*100)+(l*10)+m;
-       std::cout<<"The original number is "<<i<<std::endl;
+       array[5]=(array[1]+3)%10;
+       array[6]=(array[2]+3)%10;
+       array[7]=(array[3]+3)%10;
+       array[8]=(array[4]+3)%10;
+       array[11]=array[5];
+       array[12]=array[6];
+       array[9]=array[7];
+       array[10]=array[8];
+       array[13]=(array[9]*1000)+(array[10]*100)+(array[11]*10)+array[12];
+       std::cout<<"The original number is "<<array[13]<<std::endl;
     }
     std::cout<<"Do you want to continue? "<<std::endl;
     std::cout<<"Type Y/y for yes or N/n for no: ";
